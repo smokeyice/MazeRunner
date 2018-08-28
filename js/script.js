a7f43b09d16c1cf8d2412642ca679884ba9e70b3
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-28 17:13:49 +0300

    HTML file finished and Divs finished

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $("#quest1-0").click(function(){
+    $("#question1").hide();
+    $("#question2-0").show();
+  });
+
+  $("#quest1-1").click(function(){
+    $("#question1").hide();
+    $("#question2-1").show();
+  });
+
+  $("#quest1-2").click(function(){
+    $("#question1").hide();
+    $("#question2-2").show();
+  });
+
+  $("#quest1-3").click(function(){
+    $("#question1").hide();
+    $("#question2-3").show();
+  });
+
+  $("#quest2-0").click(function(){
+    $("#question1").hide();
+    $("#question2-3").show();
+
+  });
+});
