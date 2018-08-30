37caa81adf648e8701a95463e63a1770a7c49e1a
smokeyice <mwangi.ian3@gmail.com>
2018-08-30 17:07:51 +0300

    Complete Music Trivia

---
+++ README.md
@@ -0,0 +1,257 @@
+# APPLICATION NAME: Maze Runner!
+
+## DESCRIPTION
+
+This Application presents to the User a Landing page where he/she is expected to choose a category of interest out of Five Given Categories, with the click of a button.
+
+Once selected, the user types in a suitable name on a form presented to him/her, then clicks "Start Game", for the game to begin. Here, he/she is expected to choose an answer out of given multiple choices. Depending on the answer picked, corresponding questions keep looping on and on until the user finds the right path to the end of the game, creating some what a Maze!
+
+## SETUP / INSTALLATION REQUIREMNTS
+
+- An Up-To-Date Browser, Preferably Google Chrome.
+
+- Internet connection
+
+- Git
+
+## SYSTEM GUIDLINES
+
+`git clone`
+
+`cd`
+
+`atom . OR code .`
+
+## KNOWN BUGS
+
+The system is running quite as expected. No errors found yet.
+
+## BEHAVIOUR DRIVEN DEVELOPMENT
+
+The following is a just one of the BDDs of the system as they can be very lengthy.
+
+Button Value > Button ID > DIV ID
+
+Level 1 Buttons
+
+Shade 45 > quest1-0 > question1 > question2-0
+Snoop > quest1-1 > question1 > question2-1
+Eminem > quest1-2 > question1 > question2-2
+Yela > quest1-3 > question1 > question2-3
+
+Level 2 Buttons
+
+Black > quest2-0 > question2-0 > question3-1
+Reasonable > quest2-1 > question2-0 > question3-0
+
+Faith Ev. > quest2-2 > question2-1 > question3-2
+Lil Kim > quest2-3 > question2-1 > question3-3
+
+Omarion > quest2-4 > question2-2 > question3-5
+Gudda > quest2-5 > question2-2 > question3-4
+
+Diddy > quest2-6 > question2-3 > question3-6
+Waka > quest2-7 > question2-3 > question3-7
+
+Level 3 Buttons
+
+JID > quest3-0> question3-0 > question4-0
+Yatchy > quest3-1 > question3-0 > question4-1
+
+Get Rich > quest3-2> question3-1 > question4-2
+Self-Destr > quest3-3> question3-1 > question4-3
+
+Jay-Z > quest3-4> question3-2 > question4-5
+Common > quest3-5> question3-2 > question4-4
+
+Illmatic > quest3-6> question3-3 > question4-6
+SpeakerBox > quest3-7> question3-3 > question4-7
+
+Bas > quest3-8> question3-4 > question4-9
+RichKid > quest3-9> question3-4 > question4-8
+
+Fresh Prince > quest3-10> question3-5 > question4-10
+DMC > quest3-11> question3-5 > question4-11
+
+JME > quest3-12> question3-6 > question4-13
+Carti > quest3-13> question3-6 > question4-12
+
+Robin > quest3-14> question3-7 > question4-14
+Dwayne > quest3-15> question3-7 > question4-15
+
+Level 4 Buttons
+
+T-Pain > quest4.1> question4-0> question5-0
+B.o.B > quest4.2> question4-0> question5-1
+
+Big26 > quest4.3> question4-1> question5-3
+Big24 > quest4.4> question4-1> question5-2
+
+MIMs > quest4.5> question4-2> question5-5
+Tpain > quest4.6> question4-2> question5-4
+
+MileyTrue > quest4.7> question4-3> question5-6
+MileyFalse > quest4.8> question4-3> question5-7
+
+WesleyTrue > quest4.9> question4-4> question5-8
+WesleyFalse > quest4.10> question4-4> question5-9
+
+Ghana > quest4.11> question4-5> question5-11
+S.A > quest4.12> question4-5> question5-10
+
+Ogopa > quest4.13> question4-6> question5-12
+Calif > quest4.14> question4-6> question5-13
+
+Wahu > quest4.15> question4-7> question5-15
+Amani > quest4.16> question4-7> question5-14
+
+Wyre > quest4.17> question4-8> question5-17
+Eric > quest4.18> question4-8> question5-16
+
+BigPin > quest4.19> question4-9> question5-19
+KRupt > quest4.20> question4-9> question5-18
+
+Bamzi > quest4.21> question4-10> question5-20
+Nazi > quest4.22> question4-10> question5-21
+
+Kid-K > quest4.23> question4-11> question5-23
+Maddy > quest4.24> question4-11> question5-22
+
+megaRider > quest4.25> question4-12> question5-24
+Sinzia > quest4.26> question4-12> question5-25
+
+Gwash > quest4.27> question4-13> question5-27
+Ethic > quest4.28> question4-13> question5-26
+
+JuaCali > quest4.29> question4-14> question5-29
+Bamzi > quest4.30> question4-14> question5-28
+
+Karuna > quest4.31> question4-15> question5-31
+Kwesta > quest4.32> question4-15> question5-30
+
+Level 5 Buttons:
+
+Dancehall > quest5.0> question5.0>
+RnB > quest5.1> question5-0>
+
+Pink > quest5.2> question5.1>
+Sia > quest5.3> question5-1>
+
+Adele > quest5.4> question5.2>
+Sam > quest5.5> question5-2>
+
+Jay-Z > quest5.6> question5.3>
+Kanye > quest5.7> question5-3>
+
+Carlos > quest5.8> question5.4>
+Jimi > quest5.9> question5-4>
+
+Vamps > quest5.10> question5.5>
+Miley > quest5.11> question5-5>
+
+J-Hus > quest5.12> question5.6>
+Skepta > quest5.13> question5-6>
+
+CBiz > quest5.14> question5.7>
+JME > quest5.15> question5-7>
+
+Sheeran > quest5.16> question5.8>
+Drake > quest5.17> question5-8>
+
+Cudi > quest5.18> question5.9>
+Rocky > quest5.19> question5-9>
+
+BlackFri > quest5.20> question5.10>
+Redemption > quest5.21> question5-10>
+
+Cozz > quest5.22> question5.11>
+Cole > quest5.23> question5-11>
+
+Kaka > quest5.24> question5.12>
+Bonga > quest5.25> question5-12>
+
+Mdundo > quest5.26> question5.13>
+WyreMusic > quest5.27> question5-13>
+
+Avril > quest5.28> question5.14>
+Wangechi > quest5.29> question5-14>
+
+VybeCult > quest5.30> question5.15>
+NuNai > quest5.31> question5-15>
+
+Tunji > quest5.32> question5.16>
+Kagwe > quest5.33> question5-16>
+
+JumpOff > quest5.34> question5.17>
+GMITM > quest5.35> question5-17>
+
+CodeRed > quest5.36> question5.18>
+Calif > quest5.37> question5-18>
+
+Pazeni > quest5.38> question5.19>
+SautiAcad > quest5.39> question5-19>
+
+Amani > quest5.40> question5.20>
+Collo > quest5.41> question5-20>
+
+Fena > quest5.42> question5.21>
+Femi > quest5.43> question5-21>
+
+EAWave > quest5.44> question5.22>
+Cosmic > quest5.45> question5-22>
+
+Khali > quest5.46> question5.23>
+octo > quest5.47> question5-23>
+
+House > quest5.48> question5.24>
+Jazz > quest5.49> question5-24>
+
+MajorLez > quest5.50> question5.25>
+Static > quest5.51> question5-25>
+
+Tory > quest5.52> question5.26>
+Jimmy > quest5.53> question5-26>
+
+Wstrn2 > quest5.54> question5.27>
+Wstrn3 > quest5.55> question5-27>
+
+MLez2 > quest5.56> question5.28>
+MLez3 > quest5.57> question5-28>
+
+Magz > quest5.58> question5.29>
+Kwesta > quest5.59> question5-29>
+
+AKA > quest5.60> question5.30>
+Kwesta > quest5.61> question5-30>
+
+LogombaTrio > quest5.62> question5.31>
+LogombaDuo > quest5.63> question5-31>
+
+## TECHNOLOGIES USED
+
+- HTML
+
+- CSS
+
+- Javascript
+
+- jQuery
+
+- Bootstrap
+
+## LIVE DEMO
+
+To view the LIVE SITE, Click Here:
+
+## SUPPORT AND CONTACT DETAILS
+
+In case of any queries, feel free to reach any of us through the following emails: mwangi.ian3@gmail.com , donald.k.kiplagat@gmail.com , njoroge.pitz@gmail.com , kmbaria.george@gmail.com , briankimani013@gmail.com
+
+## COPYRIGHT & LICENCE
+
+<2018> Ian Mwangi.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ js/script1.js
@@ -0,0 +1,628 @@
+// $("#btnPlay").click(function(){
+//   $("#formBody").hide();
+//   $("#mainContent").show;
+// });
+
+$(document).ready(function() {
+  //LEVEL 1 BUTTONS
+  $("#quest1-0").click(function() {
+    $("#question1").hide();
+    $("#question2-0").show();
+  });
+  $("#quest1-1").click(function() {
+    $("#question1").hide();
+    $("#question2-1").show();
+  });
+  $("#quest1-2").click(function() {
+    $("#question1").hide();
+    $("#question2-2").show();
+  });
+  $("#quest1-3").click(function() {
+    $("#question1").hide();
+    $("#question2-3").show();
+  });
+
+  //LEVEL 2 BUTTONS
+
+  $("#quest2-0").click(function() {
+    $("#question2-0").hide();
+    $("#question3-1").show();
+  });
+  $("#quest2-1").click(function() {
+    $("#question2-0").hide();
+    $("#question3-0").show();
+  });
+
+  $("#quest2-2").click(function() {
+    $("#question2-1").hide();
+    $("#question3-2").show();
+  });
+
+  $("#quest2-3").click(function() {
+    $("#question2-1").hide();
+    $("#question3-3").show();
+  });
+
+  $("#quest2-4").click(function() {
+    $("#question2-2").hide();
+    $("#question3-5").show();
+  });
+
+  $("#quest2-5").click(function() {
+    $("#question2-2").hide();
+    $("#question3-4").show();
+  });
+
+  $("#quest2-6").click(function() {
+    $("#question2-3").hide();
+    $("#question3-6").show();
+  });
+
+  $("#quest2-7").click(function() {
+    $("#question2-3").hide();
+    $("#question3-7").show();
+  });
+
+  //LEVEL 3 BUTTONS:
+  $("#quest3-0").click(function() {
+    $("#question3-0").hide();
+    $("#question4-0").show();
+  });
+
+  $("#quest3-1").click(function() {
+    $("#question3-0").hide();
+    $("#question4-1").show();
+  });
+
+  $("#quest3-2").click(function() {
+    $("#question3-1").hide();
+    $("#question4-2").show();
+  });
+
+  $("#quest3-3").click(function() {
+    $("#question3-1").hide();
+    $("#question4-3").show();
+  });
+
+  $("#quest3-4").click(function() {
+    $("#question3-2").hide();
+    $("#question4-5").show();
+  });
+
+  $("#quest3-5").click(function() {
+    $("#question3-2").hide();
+    $("#question4-4").show();
+  });
+
+  $("#quest3-6").click(function() {
+    $("#question3-3").hide();
+    $("#question4-6").show();
+  });
+
+  $("#quest3-7").click(function() {
+    $("#question3-3").hide();
+    $("#question4-7").show();
+  });
+
+  $("#quest3-8").click(function() {
+    $("#question3-4").hide();
+    $("#question4-9").show();
+  });
+
+  $("#quest3-9").click(function() {
+    $("#question3-4").hide();
+    $("#question4-8").show();
+  });
+
+  $("#quest3-10").click(function() {
+    $("#question3-5").hide();
+    $("#question4-10").show();
+  });
+
+  $("#quest3-11").click(function() {
+    $("#question3-5").hide();
+    $("#question4-11").show();
+  });
+
+  $("#quest3-12").click(function() {
+    $("#question3-6").hide();
+    $("#question4-13").show();
+  });
+
+  $("#quest3-13").click(function() {
+    $("#question3-6").hide();
+    $("#question4-12").show();
+  });
+
+  $("#quest3-14").click(function() {
+    $("#question3-7").hide();
+    $("#question4-14").show();
+  });
+
+  $("#quest3-15").click(function() {
+    $("#question3-7").hide();
+    $("#question4-15").show();
+  });
+
+  //LEVEL 4 BUTTONS:
+  $("#quest4-1").click(function() {
+    $("#question4-0").hide();
+    $("#question5-0").show();
+  });
+
+  $("#quest4-2").click(function() {
+    $("#question4-0").hide();
+    $("#question5-1").show();
+  });
+
+  $("#quest4-3").click(function() {
+    $("#question4-1").hide();
+    $("#question5-3").show();
+  });
+
+  $("#quest4-4").click(function() {
+    $("#question4-1").hide();
+    $("#question5-2").show();
+  });
+
+  $("#quest4-5").click(function() {
+    $("#question4-2").hide();
+    $("#question5-5").show();
+  });
+
+  $("#quest4-6").click(function() {
+    $("#question4-2").hide();
+    $("#question5-4").show();
+  });
+
+  $("#quest4-7").click(function() {
+    $("#question4-3").hide();
+    $("#question5-6").show();
+  });
+
+  $("#quest4-8").click(function() {
+    $("#question4-3").hide();
+    $("#question5-7").show();
+  });
+
+  $("#quest4-9").click(function() {
+    $("#question4-4").hide();
+    $("#question5-8").show();
+  });
+
+  $("#quest4-10").click(function() {
+    $("#question4-4").hide();
+    $("#question5-9").show();
+  });
+
+  $("#quest4-11").click(function() {
+    $("#question4-5").hide();
+    $("#question5-11").show();
+  });
+
+  $("#quest4-12").click(function() {
+    $("#question4-5").hide();
+    $("#question5-10").show();
+  });
+
+  $("#quest4-13").click(function() {
+    $("#question4-6").hide();
+    $("#question5-12").show();
+  });
+  $("#quest4-14").click(function() {
+    $("#question4-6").hide();
+    $("#question5-13").show();
+  });
+
+  $("#quest4-15").click(function() {
+    $("#question4-7").hide();
+    $("#question5-15").show();
+  });
+
+  $("#quest4-16").click(function() {
+    $("#question4-7").hide();
+    $("#question5-14").show();
+  });
+
+  $("#quest4-17").click(function() {
+    $("#question4-8").hide();
+    $("#question5-17").show();
+  });
+
+  $("#quest4-18").click(function() {
+    $("#question4-8").hide();
+    $("#question5-16").show();
+  });
+
+  $("#quest4-19").click(function() {
+    $("#question4-9").hide();
+    $("#question5-19").show();
+  });
+
+  $("#quest4-20").click(function() {
+    $("#question4-9").hide();
+    $("#question5-18").show();
+  });
+
+  $("#quest4-21").click(function() {
+    $("#question4-10").hide();
+    $("#question5-20").show();
+  });
+
+  $("#quest4-22").click(function() {
+    $("#question4-10").hide();
+    $("#question5-21").show();
+  });
+
+  $("#quest4-23").click(function() {
+    $("#question4-11").hide();
+    $("#question5-23").show();
+  });
+
+  $("#quest4-24").click(function() {
+    $("#question4-11").hide();
+    $("#question5-22").show();
+  });
+
+  $("#quest4-25").click(function() {
+    $("#question4-12").hide();
+    $("#question5-24").show();
+  });
+
+  $("#quest4-26").click(function() {
+    $("#question4-12").hide();
+    $("#question5-25").show();
+  });
+
+  $("#quest4-27").click(function() {
+    $("#question4-13").hide();
+    $("#question5-27").show();
+  });
+
+  $("#quest4-28").click(function() {
+    $("#question4-13").hide();
+    $("#question5-26").show();
+  });
+
+  $("#quest4-29").click(function() {
+    $("#question4-14").hide();
+    $("#question5-29").show();
+  });
+
+  $("#quest4-30").click(function() {
+    $("#question4-14").hide();
+    $("#question5-28").show();
+  });
+
+  $("#quest4-31").click(function() {
+    $("#question4-15").hide();
+    $("#question5-31").show();
+  });
+
+  $("#quest4-32").click(function() {
+    $("#question4-15").hide();
+    $("#question5-30").show();
+  });
+
+  //LEVEL 5 BUTTONS:
+
+  $("#quest5-0").click(function() {
+    $("#question5-0").hide();
+    $("#question2-0").show();
+  });
+
+  $("#quest5-1").click(function() {
+    $("#question5-0").hide();
+    $("#question2-1").show();
+  });
+
+  $("#quest5-2").click(function() {
+    $("#question5-1").hide();
+    $("#question1-0").show();
+  });
+
+  $("#quest5-3").click(function() {
+    $("#question5-1").hide();
+    $("#question3-5").show();
+  });
+
+  $("#quest5-4").click(function() {
+    $("#question5-2").hide();
+    $("#question4-1").show();
+  });
+
+  $("#quest5-5").click(function() {
+    $("#question5-2").hide();
+    $("#question4-6").show();
+  });
+
+  $("#quest5-6").click(function() {
+    $("#question5-3").hide();
+    $("#question2-2").show();
+  });
+
+  $("#quest5-7").click(function() {
+    $("#question5-3").hide();
+    $("#question2-3").show();
+  });
+
+  $("#quest5-8").click(function() {
+    $("#question5-4").hide();
+    $("#question3-0").show();
+  });
+
+  $("#quest5-9").click(function() {
+    $("#question5-4").hide();
+    $("#question4-1").show();
+  });
+
+  $("#quest5-10").click(function() {
+    $("#question5-5").hide();
+    $("#question3-0").show();
+  });
+
+  $("#quest5-11").click(function() {
+    $("#question5-5").hide();
+    $("#question5-3-2").show();
+  });
+
+  $("#quest5-12").click(function() {
+    $("#question5-6").hide();
+    $("#question4-13").show();
+  });
+
+  $("#quest5-13").click(function() {
+    $("#question5-6").hide();
+    $("#question4-14").show();
+  });
+
+  $("#quest5-14").click(function() {
+    $("#question5-7").hide();
+    $("#question5-2-3").show();
+  });
+
+  $("#quest5-15").click(function() {
+    $("#question5-7").hide();
+    $("#question3-3").show();
+  });
+
+  $("#quest5-16").click(function() {
+    $("#question5-8").hide();
+    $("#question3-5").show();
+  });
+
+  $("#quest5-17").click(function() {
+    $("#question5-8").hide();
+    $("#question3-7").show();
+  });
+
+  $("#quest5-18").click(function() {
+    $("#question5-9").hide();
+    $("#question3-6").show();
+  });
+
+  $("#quest5-19").click(function() {
+    $("#question5-9").hide();
+    $("#question4-0").show();
+  });
+
+  $("#quest5-20").click(function() {
+    $("#question5-10").hide();
+    $("#question4-13").show();
+  });
+
+  $("#quest5-21").click(function() {
+    $("#question5-10").hide();
+    $("#question4-11").show();
+  });
+
+  $("#quest5-22").click(function() {
+    $("#question5-11").hide();
+    $("#question3-2").show();
+  });
+
+  $("#quest5-23").click(function() {
+    $("#question5-11").hide();
+    $("#question3-3").show();
+  });
+
+  $("#quest5-24").click(function() {
+    $("#question5-12").hide();
+    $("#question3-4").show();
+  });
+
+  $("#quest5-25").click(function() {
+    $("#question5-12").hide();
+    $("#question4-10").show();
+  });
+
+  $("#quest5-26").click(function() {
+    $("#question5-13").hide();
+    $("#question4-14").show();
+  });
+
+  $("#quest5-27").click(function() {
+    $("#question5-13").hide();
+    $("#question4-15").show();
+  });
+
+  $("#quest5-28").click(function() {
+    $("#question5-14").hide();
+    $("#question3-6").show();
+  });
+
+  $("#quest5-29").click(function() {
+    $("#question5-14").hide();
+    $("#question4-3").show();
+  });
+
+  $("#quest5-30").click(function() {
+    $("#question5-15").hide();
+    $("#question4-2").show();
+  });
+
+  $("#quest5-31").click(function() {
+    $("#question5-15").hide();
+    $("#question4-11").show();
+  });
+
+  $("#quest5-32").click(function() {
+    $("#question5-16").hide();
+    $("#question4-0").show();
+  });
+
+  $("#quest5-33").click(function() {
+    $("#question5-16").hide();
+    $("#question3-4").show();
+  });
+
+  $("#quest5-34").click(function() {
+    $("#question5-17").hide();
+    $("#question3-0").show();
+  });
+
+  $("#quest5-35").click(function() {
+    $("#question5-17").hide();
+    $("#question2-3").show();
+  });
+
+  $("#quest5-36").click(function() {
+    $("#question5-18").hide();
+    $("#question4-3").show();
+  });
+
+  $("#quest5-37").click(function() {
+    $("#question5-18").hide();
+    $("#question4-9").show();
+  });
+
+  $("#quest5-38").click(function() {
+    $("#question5-19").hide();
+    $("#question4-8").show();
+  });
+
+  $("#quest5-39").click(function() {
+    $("#question5-19").hide();
+    $("#question4-7").show();
+  });
+
+  $("#quest5-40").click(function() {
+    $("#question5-20").hide();
+    $("#win").show();
+  });
+
+  $("#quest5-41").click(function() {
+    $("#question5-20").hide();
+    $("#question4-1").show();
+  });
+
+  $("#quest5-42").click(function() {
+    $("#question5-21").hide();
+    $("#question3-0").show();
+  });
+
+  $("#quest5-43").click(function() {
+    $("#question5-21").hide();
+    $("#question2-2").show();
+  });
+
+  $("#quest5-44").click(function() {
+    $("#question5-22").hide();
+    $("#question3-4").show();
+  });
+
+  $("#quest5-45").click(function() {
+    $("#question5-22").hide();
+    $("#question4-4").show();
+  });
+
+  $("#quest5-46").click(function() {
+    $("#question5-23").hide();
+    $("#question4-3").show();
+  });
+
+  $("#quest5-47").click(function() {
+    $("#question5-23").hide();
+    $("#question2-1").show();
+  });
+
+  $("#quest5-48").click(function() {
+    $("#question5-24").hide();
+    $("#question3-6").show();
+  });
+
+  $("#quest5-49").click(function() {
+    $("#question5-24").hide();
+    $("#question3-6").show();
+  });
+
+  $("#quest5-50").click(function() {
+    $("#question5-25").hide();
+    $("#question4-11").show();
+  });
+
+  $("#quest5-51").click(function() {
+    $("#question5-25").hide();
+    $("#question4-10").show();
+  });
+
+  $("#quest5-52").click(function() {
+    $("#question5-26").hide();
+    $("#question4-7").show();
+  });
+
+  $("#quest5-53").click(function() {
+    $("#question5-26").hide();
+    $("#question4-5").show();
+  });
+
+  $("#quest5-54").click(function() {
+    $("#question5-27").hide();
+    $("#question4-11").show();
+  });
+
+  $("#quest5-55").click(function() {
+    $("#question5-27").hide();
+    $("#question4-14").show();
+  });
+
+  $("#quest5-56").click(function() {
+    $("#question5-28").hide();
+    $("#question3-2").show();
+  });
+
+  $("#quest5-57").click(function() {
+    $("#question5-28").hide();
+    $("#question4-12").show();
+  });
+
+  $("#quest5-58").click(function() {
+    $("#question5-29").hide();
+    $("#question4-12").show();
+  });
+
+  $("#quest5-59").click(function() {
+    $("#question5-29").hide();
+    $("#question4-10").show();
+  });
+
+  $("#quest5-60").click(function() {
+    $("#question5-30").hide();
+    $("#question2-3").show();
+  });
+
+  $("#quest5-61").click(function() {
+    $("#question5-30").hide();
+    $("#question2-1").show();
+  });
+
+  $("#quest5-62").click(function() {
+    $("#question5-31").hide();
+    $("#question2-2").show();
+  });
+
+  $("#quest5-63").click(function() {
+    $("#question5-").hide();
+    $("#question3-7").show();
+  });
+});
